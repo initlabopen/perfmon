40bc2ba9a4fdc3a6ec3fc8ac31e51236f9ab5ac8
drupaladmin <drupaladmin@1942648.no-reply.drupal.org>
2016-09-07 13:03:37 +0300

    move perfmon.test.php to module folder

---
+++ perfmon.test.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DRUPAL_ROOT', $_SERVER['DOCUMENT_ROOT']);
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+
+drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
+drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
+
+?>
