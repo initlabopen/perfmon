58644a9bab298f997b3191a3c85d001f3fa0df9b
drupaladmin <drupaladmin@1942648.no-reply.drupal.org>
2017-05-11 14:59:34 +0300

    fix code to drupal standards

--- perfmon.test.php
+++ perfmon.test.php
@@ -1,9 +1,13 @@
 <?php
 
+/**
+ * @file
+ * Simple bootstrap drupal for test.
+ */
+
 define('DRUPAL_ROOT', $_SERVER['DOCUMENT_ROOT']);
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 
 drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
 drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
-
 ?>
